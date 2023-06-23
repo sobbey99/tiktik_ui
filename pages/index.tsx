8e4f255955cbdda3e324ef074f4020728845894b
sobbey99 <sobbey99@gmail.com>
2023-06-23 21:30:17 +0300

    main video view, SSR videos

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,16 @@
+import axios from "axios";
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ videos }) => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: data,
+  };
 };
 
 export default Home;
