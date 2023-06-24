ca596d2d4431ba998eb6f1e9de12983ba2e6f178
sobbey99 <sobbey99@gmail.com>
2023-06-24 15:05:07 +0300

    search  accounts

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -14,15 +14,20 @@ import useAuthStore from "../../store/authStore";
 const Search = ({ videos }: { videos: Video[] }) => {
   const [isAccounts, setIsAccounts] = useState(false);
   const router = useRouter();
+  const { allUsers } = useAuthStore();
 
-  const { searchTerm } = router.query;
+  const { searchTerm }: any = router.query;
 
   const accounts = isAccounts ? "border-b-2 border-black" : "text-gray-400";
   const isVideos = !isAccounts ? "border-b-2 border-black" : "text-gray-400";
 
+  const searchedAccounts = allUsers.filter((user: IUser) =>
+    user.userName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="w-full">
-      <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+      <div className="flex gap-10  mt-10 border-b-2 border-gray-200 bg-white w-full">
         <p
           onClick={() => setIsAccounts(true)}
           className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`}
@@ -37,7 +42,37 @@ const Search = ({ videos }: { videos: Video[] }) => {
         </p>
       </div>
       {isAccounts ? (
-        <div>ACCOUNTS</div>
+        <div className="md-mt-16 mt-3">
+          {searchedAccounts.length > 0 ? (
+            searchedAccounts.map((user: IUser, idx: number) => (
+              <Link href={`/profile/${user._id}`} key={idx}>
+                <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
+                  <div>
+                    <Image
+                      src={user.image}
+                      width={50}
+                      height={50}
+                      className="rounded-full"
+                      alt="user profile"
+                    />
+                  </div>
+
+                  <div className="hidden xl:block">
+                    <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                      {user.userName.replaceAll(" ", "")}
+                      <GoVerified className="text-blue-400" />
+                    </p>
+                    <p className="capitalize text-gray-400 text-xs">
+                      {user.userName}
+                    </p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <NoResults text={`No video results for ${searchTerm}`} />
+          )}
+        </div>
       ) : (
         <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
           {videos.length ? (
