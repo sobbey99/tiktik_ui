5449abc1abfd49e781039febd55842f02081c7bc
sobbey99 <sobbey99@gmail.com>
2023-06-24 14:56:05 +0300

    search video

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,9 +9,21 @@ import Logo from "../utils/tiktik-logo.png";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { createOrGetUser } from "../utils";
 import useAuthStore from "../store/authStore";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const { userProfile, addUser, removeUser } = useAuthStore();
+  const [searchValue, setSearchValue] = useState("");
+
+  const router = useRouter();
+
+  const handleSearch = (e: any) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -24,7 +36,27 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>SEARCH</div>
+
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search accounts and videos"
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 width-[300px] md:w-[350px] rounded-full md:top-0"
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
 
       {userProfile ? (
         <div className="flex gap-5 md:gap-10 items-center">
---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,70 @@
+import axios from "axios";
+import { BASE_URL } from "../../utils";
+import { useState } from "react";
+import Image from "next/legacy/image";
+import { GoVerified } from "react-icons/go";
+
+import VideoCard from "../../components/VideoCard";
+import NoResults from "../../components/NoResults";
+import { IUser, Video } from "../../types";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import useAuthStore from "../../store/authStore";
+
+const Search = ({ videos }: { videos: Video[] }) => {
+  const [isAccounts, setIsAccounts] = useState(false);
+  const router = useRouter();
+
+  const { searchTerm } = router.query;
+
+  const accounts = isAccounts ? "border-b-2 border-black" : "text-gray-400";
+  const isVideos = !isAccounts ? "border-b-2 border-black" : "text-gray-400";
+
+  return (
+    <div className="w-full">
+      <div className="flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full">
+        <p
+          onClick={() => setIsAccounts(true)}
+          className={`text-xl font-semibold cursor-pointer mt-2 ${accounts}`}
+        >
+          Accounts
+        </p>
+        <p
+          onClick={() => setIsAccounts(false)}
+          className={`text-xl font-semibold cursor-pointer mt-2 ${isVideos}`}
+        >
+          Videos
+        </p>
+      </div>
+      {isAccounts ? (
+        <div>ACCOUNTS</div>
+      ) : (
+        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start">
+          {videos.length ? (
+            videos.map((video: Video, idx: number) => (
+              <VideoCard post={video} key={idx} />
+            ))
+          ) : (
+            <NoResults text={`No video results for ${searchTerm}`} />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async ({
+  params: { searchTerm },
+}: {
+  params: { searchTerm: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
+
+  return {
+    props: {
+      videos: res.data,
+    },
+  };
+};
+
+export default Search;
