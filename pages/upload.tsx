c87ec7f9009c9f52b47464b1bb90356e56e1eee2
sobbey99 <sobbey99@gmail.com>
2023-06-24 15:41:16 +0300

    upload adopted

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -69,8 +69,8 @@ const Upload = () => {
     }
   };
   return (
-    <div className="flex w-full h-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
-      <div className="bg-white rounded-lg xl:h-[80vh] flex gap-6 w-[60%] flex-wrap justify-between items-center p-14 pt-6">
+    <div className="flex w-full absolute left-0 top-[60px] mb-10 pt-10 lg:pt-20 bg-[#F8F8F8] justify-center">
+      <div className="bg-white rounded-lg xl:h-[80vh] flex gap-6 w-[60%] flex-wrap justify-between items-center p-14 pt-6 upload-adopted">
         <div>
           <div>
             <p className="text-2xl font-bold">Upload Video</p>
@@ -78,7 +78,7 @@ const Upload = () => {
               Post a video to your account
             </p>
           </div>
-          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100">
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-1 w-[260px] h-[70%] p-3 cursor-pointer hover:border-red-300 hover:bg-gray-100">
             {isLoading ? (
               <p>Uploading...</p>
             ) : (
